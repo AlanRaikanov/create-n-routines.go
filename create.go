ec30463c1079e94718617d0bd7dd3aa1e2d93f7c
AlanRaikanov <raikanov.alan@gmail.com>
2021-12-07 19:47:12 +0600

    added goroutines waiter

--- create.go
+++ create.go
@@ -3,22 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
-	n := flag.Int("n", 10, "Number of goroutines")
+	n := flag.Int("n", 20, "Number of goroutines")
 	flag.Parse()
-
 	count := *n
 	fmt.Printf("Going to create %d number of goroutines.\n", count)
 
+	var waitGroup sync.WaitGroup
+	fmt.Printf("%#v\n", waitGroup)
 	for i := 0; i < count; i++ {
+		waitGroup.Add(1)
 		go func(x int) {
+			defer waitGroup.Done()
 			fmt.Printf("%d ", x)
 		}(i)
 	}
-
-	time.Sleep(time.Second)
+	fmt.Printf("%#v\n", waitGroup)
+	waitGroup.Wait()
 	fmt.Println("\nExiting...")
 }
